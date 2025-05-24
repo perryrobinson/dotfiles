6305b350b6bc19ce4f242cf780b08ccc86964d92
Perry Robinson <perryrobinson68@gmail.com>
2025-05-24 15:38:57 -0500

    update node config with yarn path var

--- bash/tool_configs/node.sh
+++ bash/tool_configs/node.sh
@@ -6,3 +6,5 @@ if [ -s "$NVM_DIR/nvm.sh" ]; then
     source "$NVM_DIR/nvm.sh"  # This loads nvm
     source "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
+
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
